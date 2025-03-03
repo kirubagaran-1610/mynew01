1b5b8de97a6eede28fa119ee676160a9bd8cbb58
kirubagaran-1610 <kirubagaranritha1610@gmail.com>
2025-03-03 23:18:02 +0530

    My new Py file

---
+++ index.py
@@ -0,0 +1,11 @@
+# Program to print Hello, World!
+print("Hello, World!")
+
+# Program to print the 2's Multiplication Table
+def print_table_2():
+    print("\nMultiplication Table of 2:")
+    for i in range(1, 11):
+        print(f"2 x {i} = {2 * i}")
+
+# Call the function
+print_table_2()
